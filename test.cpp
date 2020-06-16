8cf5f0a0c6b17428b63201535f551cedb33eb632
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2020-06-16 13:50:17 +0200

    working EraseAll

---
+++ test.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <typeinfo>
+#include <typeindex>
+
+#include "typelist.hpp"
+
+using ExampleType1 = TypeList<int>;
+using ExampleType2 = TypeList<int, long int, char>;
+using ExampleType3 = TypeList<int, int, int, int>;
+
+using ExampleType4 = TypeList<ExampleType1, ExampleType2>;
+
+using ExampleType5 = TypeList<int, long int, int, bool, int, char>;
+
+int main() {
+	std::cout << TypeList<>::size << "\n";
+	std::cout << ExampleType1::size << "\n";
+	std::cout << ExampleType2::size << "\n";
+	std::cout << ExampleType3::size << "\n";
+	std::cout << ExampleType4::size << "\n";
+	std::cout << "\n";
+	typename TypeAt<ExampleType3, 0>::Result i;
+	i = 7;
+	std::cout << "i = " << i << "\n";
+	//std::cout << decltype(i).name() << "\n";
+	std::cout << "\n";
+	std::cout << typeid(int).name() << "\n";
+	std::cout << typeid(int).hash_code() << "\n";
+	std::cout << (std::type_index(typeid(int)) == std::type_index(typeid(TypeAt<ExampleType3, 0>::Result))) << "\n";
+	std::cout << typeid(TypeAt<ExampleType3, 0>::Result).name() << "\n";
+	std::cout << (typeid(TypeAt<ExampleType3, 0>::Result).hash_code() == typeid(int).hash_code()) << "\n";
+	std::cout << (typeid(TypeAt<ExampleType3, 0>::Result) == typeid(int)) << "\n";
+	std::cout << "\n";
+	typename TypeAt<ExampleType2, 2>::Result c;
+	c = 'a';
+	std::cout << "c = " << c << "\n";
+	std::cout << "\n";
+	std::cout << Append<TypeList<>, double>::Result::size << "\n";
+	std::cout << Append<TypeList<int>, double>::Result::size << "\n";
+	std::cout << Append<TypeList<int, float>, double>::Result::size << "\n";
+	std::cout << Append<ExampleType3, double>::Result::size << "\n";
+	std::cout << "\n";
+	std::cout << IndexOf<TypeList<>, int>::value << "\n";
+	std::cout << IndexOf<ExampleType2, int>::value << "\n";
+	std::cout << IndexOf<ExampleType2, long int>::value << "\n";
+	std::cout << "\n";
+	std::cout << Erase<ExampleType2, long int>::Result::size << "\n";
+	std::cout << "\n";
+	typename CreateTuple<ExampleType2>::Result tmp;
+	std::get<0>(tmp) = 7;
+	std::get<1>(tmp) = 14L;
+	std::get<2>(tmp) = 'z';
+	std::cout << std::get<0>(tmp) << "\n";
+	std::cout << std::get<1>(tmp) << "\n";
+	std::cout << std::get<2>(tmp) << "\n";
+
+	//sprawdzenie nowych funkcji:
+	std::cout << "\n";
+	std::cout << "Lista 5 o dlugości: " << ExampleType5::size << "\n";
+	std::cout << typeid(TypeAt<ExampleType5, 0>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ExampleType5, 1>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ExampleType5, 2>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ExampleType5, 3>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ExampleType5, 4>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ExampleType5, 5>::Result).name() << "\n";
+	std::cout << "\n";
+
+	std::cout << EraseAll<TypeList<int,int,int,long,char,int, long int>,long>::Result::size << "\n";
+
+
+	std::cout << "Usuwanie duplikatow" << "\n";
+	// std::cout << NoDuplicates<ExampleType5>::Result::size << "\n";
+	// std::cout << NoDuplicates<TypeList<int,char,int,long,char,long int>>::Result::size << "\n";
+	// std::cout << typeid(TypeAt<NoDuplicates<TypeList<int,char,int,long,char,long int>>::Result, 0>::Result).name() << ", ";
+	// std::cout << typeid(TypeAt<NoDuplicates<TypeList<int,char,int,long,char,long int>>::Result, 1>::Result).name() << ", ";
+	// std::cout << typeid(TypeAt<NoDuplicates<TypeList<int,char,int,long,char,long int>>::Result, 2>::Result).name() << ", ";
+	// std::cout << typeid(TypeAt<NoDuplicates<TypeList<int,char,int,long,char,long int>>::Result, 3>::Result).name() << ", ";
+	// std::cout << typeid(TypeAt<NoDuplicates<TypeList<int,char,int,long,char,long int>>::Result, 4>::Result).name() << ", ";
+	// std::cout << typeid(TypeAt<NoDuplicates<TypeList<int,char,int,long,char,long int>>::Result, 5>::Result).name() << "\n";
+	// std::cout << typeid(TypeAt<ExampleType6, 0>::Result).name() << ", ";
+	// std::cout << typeid(TypeAt<ExampleType6, 1>::Result).name() << ", ";
+	// std::cout << typeid(TypeAt<ExampleType6, 2>::Result).name() << ", ";
+	// std::cout << typeid(TypeAt<ExampleType6, 3>::Result).name() << "\n";
+	std::cout << "\n";
+
+	return 0;
+}
+
---
+++ typelist.hpp
@@ -0,0 +1,225 @@
+#if !defined __TYPELIST_HPP__
+#define __TYPELIST_HPP__
+
+#include <tuple>
+
+class NullType {};
+
+template<typename ... Args>
+struct TypeList;
+
+
+template<typename ... Args>
+struct TypeList {
+	static const int size = sizeof...(Args);
+};
+
+//******************************************************
+
+
+template<typename ... Args>
+struct CreateTuple;
+
+template<typename ... Args>
+struct CreateTuple<TypeList<Args...> > {
+	using Result = std::tuple<Args...>;
+};
+
+
+//******************************************************
+
+
+template<typename ListOfTypes, int index>
+struct TypeAt;
+
+
+template<typename Head, typename ... Tail>
+struct TypeAt<TypeList<Head, Tail...>, 0> {
+	using Result = Head;
+};
+
+template<typename Head, typename ... Tail, int index>
+struct TypeAt<TypeList<Head, Tail...>, index> {
+	using Result = typename TypeAt<TypeList<Tail...>, index - 1>::Result;
+};
+
+
+//******************************************************
+
+
+template<typename T, typename ListOfTypes>
+struct InsertFront;
+
+
+template<typename T, typename ... Args>
+struct InsertFront<T, TypeList<Args...> > {
+	using Result = TypeList<T, Args...>;
+};
+
+
+//******************************************************
+
+
+template<typename ListOfTypes, typename T>
+struct Append;
+
+
+template<typename T>
+struct Append<TypeList<>, T> {
+	using Result = TypeList<T>;
+};
+
+template<typename Head, typename ... Tail, typename T>
+struct Append<TypeList<Head, Tail...>, T> {
+	using Result = typename InsertFront<Head, typename Append<TypeList<Tail...>, T>::Result>::Result;
+};
+
+
+//******************************************************
+
+
+template<typename T, typename ListOfTypes>
+struct IndexOf;
+
+
+template<typename T>
+struct IndexOf<TypeList<>, T> {
+	static const int value = -1;
+};
+
+template<typename Head, typename ... Tail>
+struct IndexOf<TypeList<Head, Tail...>, Head> {
+	static const int value = 0;
+};
+
+template<typename T, typename Head, typename ... Tail>
+struct IndexOf<TypeList<Head, Tail...>, T> {
+    private:
+	static const int aux_value = IndexOf<TypeList<Tail...>, T>::value;
+    public:
+	static const int value = (aux_value == -1)?(-1):(1 + aux_value);
+};
+
+
+//******************************************************
+
+
+template<typename ListOfTypes, typename T>
+struct Erase;
+
+
+template<typename T>
+struct Erase<TypeList<>, T> {
+	using Result = TypeList<>;
+};
+
+template<typename Head, typename ... Tail>
+struct Erase<TypeList<Head, Tail...>, Head> {
+	using Result = TypeList<Tail...>;
+};
+
+template<typename Head, typename ... Tail, typename T>
+struct Erase<TypeList<Head, Tail...>, T> {
+	using Result = typename InsertFront<Head, typename Erase<TypeList<Tail...>, T>::Result>::Result;
+};
+
+
+//******************************************************
+
+template<typename ListOfTypes, typename T>
+struct EraseAll;
+
+
+template<typename T>
+struct EraseAll<TypeList<>, T> {
+	using Result = TypeList<>;
+};
+
+template<typename Head, typename ... Tail>
+struct EraseAll<TypeList<Head, Tail...>, Head> {
+	using Result = typename EraseAll<TypeList<Tail...>,Head>::Result;
+};
+
+template<typename Head, typename ... Tail, typename T>
+struct EraseAll<TypeList<Head, Tail...>, T> {
+	// using Result = typename InsertFront<Head, typename EraseAll<TypeList<Tail...>, T>::Result>::Result;
+	using Result = typename InsertFront<Head,typename EraseAll<TypeList<Tail...>,T>::Result>::Result;
+};
+
+
+//******************************************************
+
+
+template<typename ListOfTypes, typename T, typename U>
+struct Replace;
+
+template <typename Head, typename U, typename ... Tail>
+struct Replace<TypeList<Head, Tail...>, Head, U> {
+	using Result = typename InsertFront< U, TypeList<Tail...> >::Result;
+};
+
+template <typename Head, typename T, typename U, typename ... Tail>
+struct Replace<TypeList<Head, Tail...>, T, U> {
+	using Result = typename InsertFront< Head, typename Replace<TypeList<Tail...>, T, U>::Result >::Result;
+};
+
+template <typename T, typename U>
+struct Replace<TypeList<>, T, U> {
+	using Result = TypeList<>;
+};
+
+
+//******************************************************
+
+
+// template <typename ListOfTypes>
+// struct NoDuplicates;
+
+// template <>
+// struct NoDuplicates<TypeList<>>
+// {
+// 	using Result = TypeList<>;
+// };
+
+// // template <typename Head>
+// // struct NoDuplicates<TypeList<Head>>
+// // {
+// // 	using Result = TypeList<Head>;
+// // };
+
+// template <typename Head, typename ...Tail>
+// struct NoDuplicates<TypeList<Head,Tail...>>
+// {
+// 	private:
+// 		using L1 = typename NoDuplicates<TypeList<Tail...>>::Result;
+// 		using L2 = typename Erase<L1, Head>::Result;
+// 	public:
+// 		using Result = InsertFront<Head, typename NoDuplicates<TypeList<L2...>>::Result>;
+// };
+
+//******************************************************
+
+
+template <class ListOfTypes>
+struct Reverse;
+
+// ...
+
+//******************************************************
+
+
+template <class T, class ListofTypes>
+struct MostDerived;
+
+// ...
+
+//******************************************************
+
+
+template <class ListOfTypes>
+struct DerivedToFront;
+
+// ...
+
+#endif
+
