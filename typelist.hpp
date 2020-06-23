ed01c8db6a1e753b0e584b6d9d6947cea31b1bbd
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2020-06-24 01:37:55 +0200

    formatowanie

--- typelist.hpp
+++ typelist.hpp
@@ -4,171 +4,168 @@
 #include <tuple>
 #include <type_traits>
 
-template<typename ... Args>
+template <typename... Args>
 struct TypeList;
 
-
-template<typename ... Args>
-struct TypeList {
+template <typename... Args>
+struct TypeList
+{
 	static const int size = sizeof...(Args);
 };
 
 //******************************************************
 
-
-template<typename ... Args>
+template <typename... Args>
 struct CreateTuple;
 
-template<typename ... Args>
-struct CreateTuple<TypeList<Args...> > {
+template <typename... Args>
+struct CreateTuple<TypeList<Args...>>
+{
 	using Result = std::tuple<Args...>;
 };
 
-
 //******************************************************
 
-
-template<typename ListOfTypes, int index>
+template <typename ListOfTypes, int index>
 struct TypeAt;
 
-
-template<typename Head, typename ... Tail>
-struct TypeAt<TypeList<Head, Tail...>, 0> {
+template <typename Head, typename... Tail>
+struct TypeAt<TypeList<Head, Tail...>, 0>
+{
 	using Result = Head;
 };
 
-template<typename Head, typename ... Tail, int index>
-struct TypeAt<TypeList<Head, Tail...>, index> {
+template <typename Head, typename... Tail, int index>
+struct TypeAt<TypeList<Head, Tail...>, index>
+{
 	using Result = typename TypeAt<TypeList<Tail...>, index - 1>::Result;
 };
 
-
 //******************************************************
 
-
-template<typename T, typename ListOfTypes>
+template <typename T, typename ListOfTypes>
 struct InsertFront;
 
-
-template<typename T, typename ... Args>
-struct InsertFront<T, TypeList<Args...> > {
+template <typename T, typename... Args>
+struct InsertFront<T, TypeList<Args...>>
+{
 	using Result = TypeList<T, Args...>;
 };
 
-
 //******************************************************
 
-
-template<typename ListOfTypes, typename T>
+template <typename ListOfTypes, typename T>
 struct Append;
 
-
-template<typename T>
-struct Append<TypeList<>, T> {
+template <typename T>
+struct Append<TypeList<>, T>
+{
 	using Result = TypeList<T>;
 };
 
-template<typename Head, typename ... Tail, typename T>
-struct Append<TypeList<Head, Tail...>, T> {
+template <typename Head, typename... Tail, typename T>
+struct Append<TypeList<Head, Tail...>, T>
+{
 	using Result = typename InsertFront<Head, typename Append<TypeList<Tail...>, T>::Result>::Result;
 };
 
-
 //******************************************************
 
-
-template<typename T, typename ListOfTypes>
+template <typename T, typename ListOfTypes>
 struct IndexOf;
 
-
-template<typename T>
-struct IndexOf<TypeList<>, T> {
+template <typename T>
+struct IndexOf<TypeList<>, T>
+{
 	static const int value = -1;
 };
 
-template<typename Head, typename ... Tail>
-struct IndexOf<TypeList<Head, Tail...>, Head> {
+template <typename Head, typename... Tail>
+struct IndexOf<TypeList<Head, Tail...>, Head>
+{
 	static const int value = 0;
 };
 
-template<typename T, typename Head, typename ... Tail>
-struct IndexOf<TypeList<Head, Tail...>, T> {
-    private:
+template <typename T, typename Head, typename... Tail>
+struct IndexOf<TypeList<Head, Tail...>, T>
+{
+private:
 	static const int aux_value = IndexOf<TypeList<Tail...>, T>::value;
-    public:
-	static const int value = (aux_value == -1)?(-1):(1 + aux_value);
-};
 
+public:
+	static const int value = (aux_value == -1) ? (-1) : (1 + aux_value);
+};
 
 //******************************************************
 
-
-template<typename ListOfTypes, typename T>
+template <typename ListOfTypes, typename T>
 struct Erase;
 
-
-template<typename T>
-struct Erase<TypeList<>, T> {
+template <typename T>
+struct Erase<TypeList<>, T>
+{
 	using Result = TypeList<>;
 };
 
-template<typename Head, typename ... Tail>
-struct Erase<TypeList<Head, Tail...>, Head> {
+template <typename Head, typename... Tail>
+struct Erase<TypeList<Head, Tail...>, Head>
+{
 	using Result = TypeList<Tail...>;
 };
 
-template<typename Head, typename ... Tail, typename T>
-struct Erase<TypeList<Head, Tail...>, T> {
+template <typename Head, typename... Tail, typename T>
+struct Erase<TypeList<Head, Tail...>, T>
+{
 	using Result = typename InsertFront<Head, typename Erase<TypeList<Tail...>, T>::Result>::Result;
 };
 
-
 //******************************************************
 
-template<typename ListOfTypes, typename T>
+template <typename ListOfTypes, typename T>
 struct EraseAll;
 
-
-template<typename T>
-struct EraseAll<TypeList<>, T> {
+template <typename T>
+struct EraseAll<TypeList<>, T>
+{
 	using Result = TypeList<>;
 };
 
-template<typename Head, typename ... Tail>
-struct EraseAll<TypeList<Head, Tail...>, Head> {
-	using Result = typename EraseAll<TypeList<Tail...>,Head>::Result;
+template <typename Head, typename... Tail>
+struct EraseAll<TypeList<Head, Tail...>, Head>
+{
+	using Result = typename EraseAll<TypeList<Tail...>, Head>::Result;
 };
 
-template<typename Head, typename ... Tail, typename T>
-struct EraseAll<TypeList<Head, Tail...>, T> {
-	using Result = typename InsertFront<Head,typename EraseAll<TypeList<Tail...>,T>::Result>::Result;
+template <typename Head, typename... Tail, typename T>
+struct EraseAll<TypeList<Head, Tail...>, T>
+{
+	using Result = typename InsertFront<Head, typename EraseAll<TypeList<Tail...>, T>::Result>::Result;
 };
-
 
 //******************************************************
 
-
-template<typename ListOfTypes, typename T, typename U>
+template <typename ListOfTypes, typename T, typename U>
 struct Replace;
 
-template <typename Head, typename U, typename ... Tail>
-struct Replace<TypeList<Head, Tail...>, Head, U> {
-	using Result = typename InsertFront< U, TypeList<Tail...> >::Result;
+template <typename Head, typename U, typename... Tail>
+struct Replace<TypeList<Head, Tail...>, Head, U>
+{
+	using Result = typename InsertFront<U, TypeList<Tail...>>::Result;
 };
 
-template <typename Head, typename T, typename U, typename ... Tail>
-struct Replace<TypeList<Head, Tail...>, T, U> {
-	using Result = typename InsertFront< Head, typename Replace<TypeList<Tail...>, T, U>::Result >::Result;
+template <typename Head, typename T, typename U, typename... Tail>
+struct Replace<TypeList<Head, Tail...>, T, U>
+{
+	using Result = typename InsertFront<Head, typename Replace<TypeList<Tail...>, T, U>::Result>::Result;
 };
 
 template <typename T, typename U>
-struct Replace<TypeList<>, T, U> {
+struct Replace<TypeList<>, T, U>
+{
 	using Result = TypeList<>;
 };
 
-
 //******************************************************
-
 
 template <typename ListOfTypes>
 struct NoDuplicates;
@@ -185,18 +182,18 @@ struct NoDuplicates<TypeList<>>
 // 	using Result = TypeList<Head>;
 // };
 
-template <typename Head, typename ...Tail>
-struct NoDuplicates<TypeList<Head,Tail...>>
+template <typename Head, typename... Tail>
+struct NoDuplicates<TypeList<Head, Tail...>>
 {
-	private:
-		using L1 = typename EraseAll<TypeList<Tail...>,Head>::Result;
-		using L2 = typename NoDuplicates<L1>::Result;
-	public:
-		using Result = typename InsertFront<Head, L2>::Result;
+private:
+	using L1 = typename EraseAll<TypeList<Tail...>, Head>::Result;
+	using L2 = typename NoDuplicates<L1>::Result;
+
+public:
+	using Result = typename InsertFront<Head, L2>::Result;
 };
 
 //******************************************************
-
 
 template <class ListOfTypes>
 struct Reverse;
@@ -207,18 +204,17 @@ struct Reverse<TypeList<>>
 	using Result = TypeList<>;
 };
 
-template <typename Head, typename ...Tail>
-struct Reverse<TypeList<Head,Tail...>>
+template <typename Head, typename... Tail>
+struct Reverse<TypeList<Head, Tail...>>
 {
-	private:
-		using L1 = typename Reverse<TypeList<Tail...>>::Result;
-	public:
-		using Result = typename Append<L1,Head>::Result;
+private:
+	using L1 = typename Reverse<TypeList<Tail...>>::Result;
+
+public:
+	using Result = typename Append<L1, Head>::Result;
 };
 
-
 //******************************************************
-
 
 template <bool flag, typename T, typename U>
 struct Select
@@ -232,36 +228,9 @@ struct Select<false, T, U>
 	using Result = U;
 };
 
-
 //******************************************************
 
-
-// namespace details {
-// 	template <typename B>
-// 	std::true_type  test_pre_ptr_convertible(const volatile B*);
-// 	template <typename>
-// 	std::false_type test_pre_ptr_convertible(const volatile void*);
-
-// 	template <typename, typename>
-// 	auto test_pre_is_base_of(...)->std::true_type;
-// 	template <typename B, typename D>
-// 	auto test_pre_is_base_of(int) ->
-// 		decltype(test_pre_ptr_convertible<B>(static_cast<D*>(nullptr)));
-// }
-
-// template <typename Base, typename Derived>
-// struct is_base_of :
-// 	std::integral_constant<
-// 	bool,
-// 	std::is_class<Base>::value && std::is_class<Derived>::value &&
-// 	decltype(details::test_pre_is_base_of<Base, Derived>(0))::value
-// 	> { };
-
-
-//******************************************************
-
-
-template < typename ListofTypes, typename T>
+template <typename ListofTypes, typename T>
 struct MostDerived;
 
 template <typename T>
@@ -270,26 +239,17 @@ struct MostDerived<TypeList<>, T>
 	using Result = T;
 };
 
-// template <typename Head, typename T>
-// struct MostDerived<TypeList<Head>, T>
-// {
-// 	using Result = typename Select<std::is_base_of<T,Head>::value,T, Head>::Result;
-// };
-
-template <typename Head, typename ...Tail, typename T>
+template <typename Head, typename... Tail, typename T>
 struct MostDerived<TypeList<Head, Tail...>, T>
 {
 private:
 	using Candidate = typename MostDerived<TypeList<Tail...>, T>::Result;
+
 public:
-// 	using Result = typename Append<TypeList<Tail...>,Select<std::is_base_of<Candidate,Head>::value, Head, Candidate>>::Result;
-	using Result = typename Select<std::is_base_of<Candidate,Head>::value,Head, Candidate>::Result;
-	// using Result = typename Select<std::is_base_of<Head,Candidate>::value,Head, Candidate>::Result;
+	using Result = typename Select<std::is_base_of<Candidate, Head>::value, Head, Candidate>::Result;
 };
 
-
 //******************************************************
-
 
 // template <class ListOfTypes>
 // struct DerivedToFront;
@@ -309,7 +269,6 @@ public:
 // public:
 // 	using Result = TypeList<TheMostDerived, L>;
 // };
-
 
 #endif
 
