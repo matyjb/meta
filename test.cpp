81028e25181884c1f7a709edb500c49f2576ae16
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2020-06-24 14:21:53 +0200

    1

--- test.cpp
+++ test.cpp
@@ -3,131 +3,115 @@
 #include <typeindex>
 
 #include "typelist.hpp"
-
-using ExampleType1 = TypeList<int>;
-using ExampleType2 = TypeList<int, long int, char>;
-using ExampleType3 = TypeList<int, int, int, int>;
-
-using ExampleType4 = TypeList<ExampleType1, ExampleType2>;
-
-using ExampleType5 = TypeList<int, long int, int, bool, int, char>;
+#include "lokiloki/Typelist.h"
+#include "lokiloki/TypelistMacros.h"
 
 class base {};
-class derived : base {};
-class mostderived2 : base {};
+class derived : public base {};
+class mostderived2 : public base {};
 class base2 {};
-class mostderived : derived, base2 {};
-class mostderived3 : base2 {};
+class mostderived : public derived, public base2 {};
+class mostderived3 : public base2 {};
 
-using ExampleType6 = TypeList<base, derived, mostderived2, mostderived, mostderived3, base2>;
+using ET1 = TypeList<int>;
+using ET1Loki = LOKI_TYPELIST_1(int);
+using ET2 = TypeList<int, long int, char>;
+using ET2Loki = LOKI_TYPELIST_3(int, long int, char);
+using ET3 = TypeList<int, int, int, int>;
+using ET3Loki = LOKI_TYPELIST_4(int, int, int, int);
+using ET4 = TypeList<char, float, int, char>;
+using ET4Loki = LOKI_TYPELIST_4(char, float, int, char);
+using ET5 = TypeList<int, long int, int, bool, int, char>;
+using ET5Loki = LOKI_TYPELIST_6(int, long int, int, bool, int, char);
+using ET6 = TypeList<base, derived, mostderived2, mostderived, mostderived3, base2>;
+using ET6Loki = LOKI_TYPELIST_6(base, derived, mostderived2, mostderived, mostderived3, base2);
 
 
 int main() {
+	std::cout << "##########################################" << "\n";
+	std::cout << "### Usuwanie duplikatow (NoDuplicates) ###" << "\n";
+	std::cout << "##########################################" << "\n";
+	std::cout << "Testowa lista: ";
+	std::cout << typeid(TypeAt<ET4, 0>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET4, 1>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET4, 2>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET4, 3>::Result).name() << "\n";
+	std::cout << "NoDuplicates (size: " << NoDuplicates<ET4>::Result::size << "):\n";
+	std::cout << typeid(TypeAt<NoDuplicates<ET4>::Result, 0>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<NoDuplicates<ET4>::Result, 1>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<NoDuplicates<ET4>::Result, 2>::Result).name() << "\n";
+	std::cout << "Loki::TL::NoDuplicates (size: " << Loki::TL::Length<Loki::TL::NoDuplicates<ET4Loki>::Result>::value << "):\n";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::NoDuplicates<ET4Loki>::Result, 0>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::NoDuplicates<ET4Loki>::Result, 1>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::NoDuplicates<ET4Loki>::Result, 2>::Result).name() << "\n";
 
-	// std::cout << TypeList<>::size << "\n";
-	// std::cout << ExampleType1::size << "\n";
-	// std::cout << ExampleType2::size << "\n";
-	// std::cout << ExampleType3::size << "\n";
-	// std::cout << ExampleType4::size << "\n";
-	// std::cout << "\n";
-	// typename TypeAt<ExampleType3, 0>::Result i;
-	// i = 7;
-	// std::cout << "i = " << i << "\n";
-	// //std::cout << decltype(i).name() << "\n";
-	// std::cout << "\n";
-	// std::cout << typeid(int).name() << "\n";
-	// std::cout << typeid(int).hash_code() << "\n";
-	// std::cout << (std::type_index(typeid(int)) == std::type_index(typeid(TypeAt<ExampleType3, 0>::Result))) << "\n";
-	// std::cout << typeid(TypeAt<ExampleType3, 0>::Result).name() << "\n";
-	// std::cout << (typeid(TypeAt<ExampleType3, 0>::Result).hash_code() == typeid(int).hash_code()) << "\n";
-	// std::cout << (typeid(TypeAt<ExampleType3, 0>::Result) == typeid(int)) << "\n";
-	// std::cout << "\n";
-	// typename TypeAt<ExampleType2, 2>::Result c;
-	// c = 'a';
-	// std::cout << "c = " << c << "\n";
-	// std::cout << "\n";
-	// std::cout << Append<TypeList<>, double>::Result::size << "\n";
-	// std::cout << Append<TypeList<int>, double>::Result::size << "\n";
-	// std::cout << Append<TypeList<int, float>, double>::Result::size << "\n";
-	// std::cout << Append<ExampleType3, double>::Result::size << "\n";
-	// std::cout << "\n";
-	// std::cout << IndexOf<TypeList<>, int>::value << "\n";
-	// std::cout << IndexOf<ExampleType2, int>::value << "\n";
-	// std::cout << IndexOf<ExampleType2, long int>::value << "\n";
-	// std::cout << "\n";
-	// std::cout << Erase<ExampleType2, long int>::Result::size << "\n";
-	// std::cout << "\n";
-	// typename CreateTuple<ExampleType2>::Result tmp;
-	// std::get<0>(tmp) = 7;
-	// std::get<1>(tmp) = 14L;
-	// std::get<2>(tmp) = 'z';
-	// std::cout << std::get<0>(tmp) << "\n";
-	// std::cout << std::get<1>(tmp) << "\n";
-	// std::cout << std::get<2>(tmp) << "\n";
+	std::cout << "##########################################" << "\n";
+	std::cout << "####### Odwracanie listy (Reverse) #######" << "\n";
+	std::cout << "##########################################" << "\n";
+	std::cout << "Testowa lista: ";
+	std::cout << typeid(TypeAt<ET5, 0>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET5, 1>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET5, 2>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET5, 3>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET5, 4>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET5, 5>::Result).name() << "\n";
+	std::cout << "Reverse (size: " << Reverse<ET5>::Result::size << "):\n";
+	std::cout << typeid(TypeAt<Reverse<ET5>::Result, 0>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<Reverse<ET5>::Result, 1>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<Reverse<ET5>::Result, 2>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<Reverse<ET5>::Result, 3>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<Reverse<ET5>::Result, 4>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<Reverse<ET5>::Result, 5>::Result).name() << "\n";
+	std::cout << "Loki::TL::Reverse (size: " << Loki::TL::Length<Loki::TL::Reverse<ET5Loki>::Result>::value << "):\n";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::Reverse<ET5Loki>::Result, 0>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::Reverse<ET5Loki>::Result, 1>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::Reverse<ET5Loki>::Result, 2>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::Reverse<ET5Loki>::Result, 3>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::Reverse<ET5Loki>::Result, 4>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::Reverse<ET5Loki>::Result, 5>::Result).name() << "\n";
 
-	//sprawdzenie nowych funkcji:
-	std::cout << "\n";
-	std::cout << "Lista 5 o dlugosci: " << ExampleType5::size << "\n";
-	std::cout << typeid(TypeAt<ExampleType5, 0>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<ExampleType5, 1>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<ExampleType5, 2>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<ExampleType5, 3>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<ExampleType5, 4>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<ExampleType5, 5>::Result).name() << "\n";
-	std::cout << "\n";
+	std::cout << "##########################################" << "\n";
+	std::cout << "### Najbardziej pochodny (MostDerived) ###" << "\n";
+	std::cout << "##########################################" << "\n";
+	std::cout << "Testowa lista: ";
+	std::cout << typeid(TypeAt<ET6, 0>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 1>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 2>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 3>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 4>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 5>::Result).name() << "\n";
+	std::cout << "(schemat dziedziczenia klas dostepny w pliku png)\n";
+	std::cout << "MostDerived<ET6, derived>: " << typeid(MostDerived<ET6, derived>::Result).name() << "\n";
+	std::cout << "Loki::TL::MostDerived<ET6, derived>: " << typeid(Loki::TL::MostDerived<ET6Loki, derived>::Result).name() << "\n";
+	std::cout << "MostDerived<ET6, base2>: " << typeid(MostDerived<ET6, base2>::Result).name() << "\n";
+	std::cout << "Loki::TL::MostDerived<ET6, base2>: " << typeid(Loki::TL::MostDerived<ET6Loki, base2>::Result).name() << "\n";
+	std::cout << "MostDerived<ET6, mostderived>: " << typeid(MostDerived<ET6, mostderived>::Result).name() << "\n";
+	std::cout << "Loki::TL::MostDerived<ET6, mostderived>: " << typeid(Loki::TL::MostDerived<ET6Loki, mostderived>::Result).name() << "\n";
 
-	std::cout << "Usuwanie wszystkich int" << "\n";
-	std::cout << EraseAll<ExampleType5,int>::Result::size << "\n";
-	std::cout << typeid(TypeAt<EraseAll<ExampleType5,int>::Result, 0>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<EraseAll<ExampleType5,int>::Result, 1>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<EraseAll<ExampleType5,int>::Result, 2>::Result).name() << "\n";
-	std::cout << "\n";
-
-	std::cout << "Usuwanie duplikatow" << "\n";
-	std::cout << NoDuplicates<ExampleType5>::Result::size << "\n";
-	std::cout << typeid(TypeAt<NoDuplicates<ExampleType5>::Result, 0>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<NoDuplicates<ExampleType5>::Result, 1>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<NoDuplicates<ExampleType5>::Result, 2>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<NoDuplicates<ExampleType5>::Result, 3>::Result).name() << "\n";
-	std::cout << "\n";
-
-	std::cout << "Odwracanie listy" << "\n";
-	std::cout << Reverse<ExampleType5>::Result::size << "\n";
-	std::cout << typeid(TypeAt<Reverse<ExampleType5>::Result, 0>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<Reverse<ExampleType5>::Result, 1>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<Reverse<ExampleType5>::Result, 2>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<Reverse<ExampleType5>::Result, 3>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<Reverse<ExampleType5>::Result, 4>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<Reverse<ExampleType5>::Result, 5>::Result).name() << "\n";
-	std::cout << "\n";
-
-	std::cout << "Najbardziej pochodny element" << "\n";
-	std::cout << typeid(MostDerived<TypeList<mostderived>, base>::Result).name() << ", ";
-	std::cout << "\n";
-	std::cout << typeid(MostDerived<TypeList<mostderived,mostderived2>, base>::Result).name() << ", ";
-	std::cout << "\n";
-	std::cout << typeid(MostDerived<TypeList<mostderived,mostderived2>, derived>::Result).name() << ", ";
-	std::cout << "\n";
-	std::cout << typeid(MostDerived<TypeList<mostderived3>, derived>::Result).name() << ", ";
-	std::cout << "\n";
-
-	std::cout << "Najbardziej pochodny element - ExampleType6" << "\n";
-	std::cout << typeid(MostDerived<ExampleType6, base>::Result).name() << ", ";
-	std::cout << typeid(MostDerived<ExampleType6, mostderived2>::Result).name() << ", ";
-	std::cout << "\n";
-
-	// std::cout << typeid(MostDerived<ExampleType6, mostderived>::Result).name() << ", ";
-	std::cout << "\n";
-
-	std::cout << "Sortowanie od najbardziej zagniezdzonych klas" << "\n";
-	std::cout << DerivedToFront<ExampleType6>::Result::size << "\n";
-	std::cout << typeid(TypeAt<DerivedToFront<ExampleType6>::Result, 0>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<DerivedToFront<ExampleType6>::Result, 1>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<DerivedToFront<ExampleType6>::Result, 2>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<DerivedToFront<ExampleType6>::Result, 3>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<DerivedToFront<ExampleType6>::Result, 4>::Result).name() << ", ";
-	std::cout << typeid(TypeAt<DerivedToFront<ExampleType6>::Result, 5>::Result).name() << "/n";
-	std::cout << "\n";
-
+	std::cout << "##########################################" << "\n";
+	std::cout << "############# DerivedToFront #############" << "\n";
+	std::cout << "##########################################" << "\n";
+	std::cout << "Testowa lista: ";
+	std::cout << typeid(TypeAt<ET6, 0>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 1>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 2>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 3>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 4>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<ET6, 5>::Result).name() << "\n";
+	std::cout << "DerivedToFront (size: " << DerivedToFront<ET6>::Result::size << "):\n";
+	std::cout << typeid(TypeAt<DerivedToFront<ET6>::Result, 0>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<DerivedToFront<ET6>::Result, 1>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<DerivedToFront<ET6>::Result, 2>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<DerivedToFront<ET6>::Result, 3>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<DerivedToFront<ET6>::Result, 4>::Result).name() << ", ";
+	std::cout << typeid(TypeAt<DerivedToFront<ET6>::Result, 5>::Result).name() << "\n";
+	std::cout << "Loki::TL::DerivedToFront (size: " << Loki::TL::Length<Loki::TL::DerivedToFront<ET6Loki>::Result>::value << "):\n";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::DerivedToFront<ET6Loki>::Result, 0>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::DerivedToFront<ET6Loki>::Result, 1>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::DerivedToFront<ET6Loki>::Result, 2>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::DerivedToFront<ET6Loki>::Result, 3>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::DerivedToFront<ET6Loki>::Result, 4>::Result).name() << ", ";
+	std::cout << typeid(Loki::TL::TypeAt<Loki::TL::DerivedToFront<ET6Loki>::Result, 5>::Result).name() << "\n";
+	
 	return 0;
 }
